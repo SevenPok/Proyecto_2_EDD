d5f5d3f87667fb93d371b4b768e8f9faecb58705
David Díaz <3006692160101@ingenieria.usac.edu.gt>
2020-06-29 17:24:07 -0600

    GraficaHash, 201807420, David Díaz

--- [EDD]Proyecto_2/src/Estructura/TablaHash/TablaHash.java
+++ [EDD]Proyecto_2/src/Estructura/TablaHash/TablaHash.java
@@ -1,4 +1,3 @@
-
 package Estructura.TablaHash;
 import Entidad.Cliente;
 
@@ -7,7 +6,7 @@ public class TablaHash {
     private int tamanio;
     private int numeroElementos;
     private double factordeCarga;
-    NodoTabla [] Tabla;
+    private NodoTabla [] Tabla;
     
     
     public TablaHash(int tamanio) {
@@ -49,9 +48,10 @@ public class TablaHash {
             factordeCarga=numeroElementos/tamanio; //actualiza la densidad en la tablahash
             if (factordeCarga>=0.75) {//verifica que no se ocupe el 75% de la tabla
                 //funcion del rehashing;
+                reHash(tamanio);
             } 
         }else if (existe(iden)) {
-            ListaDoble lista = getCliente(iden);
+            ListaDoble lista = ObtenerCliente(iden);
             if (lista.existe(cli.getDpi())==false) {
                 lista.Insertar(cli);
             }else{
@@ -81,7 +81,7 @@ public class TablaHash {
     }
     
     
-    private ListaDoble getCliente(int dpi) {
+    private ListaDoble ObtenerCliente(int dpi) {
         for (int i = 0; i < tamanio; i++) {
             if (Tabla[i] != null) {
                 if (Tabla[i].ident == dpi) {
@@ -92,6 +92,37 @@ public class TablaHash {
         return null;
     }
     
-    
+   public String graficar(TablaHash tabla){
+       String codGraph="";
+       codGraph+="rankdir = LR; \n node [shape=record];\n";
+       codGraph+="hashTable[label="+'"';
+       for (int i = 0; i <= tabla.tamanio; i++) {
+           codGraph+="<f"+i+">"+i+"|";
+       }
+       codGraph+='"'+"];\n";
+       for (int i = 0; i <tabla.tamanio; i++) {
+           NodoTabla ntabla=tabla.Tabla[i];
+           if(ntabla!=null){
+               codGraph+="node"+i+" [label="+'"';
+               ListaN auxiliar = ntabla.lista.first;
+               int contador=0;
+               while(auxiliar!=null){
+                   codGraph+="<f"+contador+"> "+auxiliar.dato.getDpi()+"|";
+                   contador++;
+                   auxiliar=auxiliar.nextt;
+               }
+               codGraph+='"'+"];\n";
+           }
+       }
+       //enlaces
+       for (int i = 0; i < tabla.tamanio; i++) {
+           NodoTabla lista2=tabla.Tabla[i];
+           if (lista2!=null) {
+               codGraph+="hashTable:f"+i+" -> node"+i+":f0;\n";
+           }
+       }
+       System.out.println(codGraph);
+       return codGraph;
+   } 
     
 }
--- [EDD]Proyecto_2/src/edd/proyecto_2/EDDProyecto_2.java
+++ [EDD]Proyecto_2/src/edd/proyecto_2/EDDProyecto_2.java
@@ -1,11 +1,19 @@
 package edd.proyecto_2;
 
-import Entidad.Conductor;
-import java.math.BigInteger;
+import Entidad.Cliente;
+import Entidad.Grafica;
+import Estructura.TablaHash.TablaHash;
+
 
 public class EDDProyecto_2 {
 
     public static void main(String[] args) {
-        
+        TablaHash tablita = new TablaHash(37);
+    Cliente cli = new Cliente(99993829,"David","Diaz","Masculino","DireccionF");
+    tablita.insertar(cli.getDpi(),cli);
+    Cliente cli2 = new Cliente(1111111111,"David","Diaz","Masculino","otra");
+    tablita.insertar(cli2.getDpi(), cli2);
+    String o= tablita.graficar(tablita);
+    Grafica.graficar(o, "ClienteHash");
     }
 }
