cfbac4ef510ea740ab973a131e537399dbccd1c8
David Díaz <3006692160101@ingenieria.usac.edu.gt>
2020-07-02 15:06:23 -0600

    busquedaViaje, 201807420, David Diaz

--- [EDD]Proyecto_2/src/Estructura/BlockChain/ListaSimpleB.java
+++ [EDD]Proyecto_2/src/Estructura/BlockChain/ListaSimpleB.java
@@ -74,4 +74,40 @@ public class ListaSimpleB {
             throw new RuntimeException(e);
         }
     }
+    
+     public String grafica(ListaSimpleB lista){
+        String graphG="";
+        graphG+="node[shape=record];\n" +
+        "graph[pencolor=transparent];\n";
+        graphG+="rankdir=LR;\n";
+        NodoBlock aux = lista.inicio;
+        System.out.println(aux.getLlave());
+        do{
+            graphG+="A"+aux.getLlave()+"[label="+'"'+"{<data> "+aux.getLlave()+"| <next>"+ "}"+'"'+"];\n";
+            aux=aux.getSiguiente();
+        }while(aux!=null);
+        //enlaces
+        NodoBlock aux2=lista.inicio;
+        do{
+            if(aux2.getSiguiente()!=null){
+            graphG+="A"+aux2.getLlave()+":next:c -> A"+aux2.getSiguiente().getLlave()+":data;\n";
+            }
+            aux2=aux2.getSiguiente();
+        }while(aux2!=null);
+        return graphG;
+    }
+     
+     public Viaje buscarViaje(String llave){
+         
+         if(inicio!=null){
+             NodoBlock aux = inicio;
+             do {
+                 if (aux.getLlave()==llave) {
+                     return aux.getViaje();
+                 }
+                 aux=aux.getSiguiente();
+             } while (aux!=null);
+         }
+        return null;
+     }
 }
